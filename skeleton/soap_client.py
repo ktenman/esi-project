3be4a98cc2a09ea64e0c3d370756ace827d0599a
Konstantin Tenman <konstantin.tenman@monese.com>
2022-03-27 15:16:42 +0300

    Fix arguments

--- skeleton/soap_client.py
+++ skeleton/soap_client.py
@@ -2,14 +2,9 @@ from suds.client import Client
 
 client = Client('http://localhost:8090/?wsdl', cache=None)
 
-# print(client.service.say_hello(u'Maarika', 5))
 print("Running Service 1 ...")
-print(client.service.ping('localhost:9000'))
+print(client.service.ping('google.com', 2))
 print("Running Service 2 ...")
-print(client.service.domain_ip('www.google.com'))
+print(client.service.domain_ip('google.com'))
 print("Running Service 3 ...")
-print(client.service.name_servers('www.google.com'))
-
-# wsdl = Client('http://www.learnwebservices.com/services/hello?wsdl', cache=None)
-# request = {'Name': 'Maarika'}
-# print(wsdl.service.SayHello(request))
+print(client.service.name_servers('google.com'))
